e0c47ccf8a4f997aff7b7cd839c79f442cc40df7
Kevin O'Hara <kevinohara80@gmail.com>
2012-04-20 20:19:38 -0400

    updated weather to remove extra line

--- examples/weather.js
+++ examples/weather.js
@@ -4,19 +4,20 @@ var elk = elknode.createConnection({ port: 2101 , host: '192.168.1.13'});
 
 elk.on('connect', function(data) {
   
-  var opts = {};
-  opts.url = 'http://query.yahooapis.com/v1/public/yql';
-  opts.qs = {};
-  opts.qs.q = 'select item from weather.forecast where location = "48307"';
-  opts.qs.format = 'json';
+  var opts = {
+    url: 'http://query.yahooapis.com/v1/public/yql',
+    qs: {
+      q: 'select item from weather.forecast where location = "48307"',
+      format: 'json'
+    }
+  };
   
   request(opts, function(error, response, body) {
     if(!error) {
       var temp = JSON.parse(body).query.results.channel.item.condition.temp.toString();    
       elk.speak('outside temperature is ' + temp + ' degrees');
     }
+    elk.disconnect();
   });
   
-}); 
-
-elk.listen();
+});
